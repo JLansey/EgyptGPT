f0781cd0fa9670759e4d4a12c3009aee75c407da
Jonathan Lansey <lansey+github@gmail.com>
2024-10-19 19:16:55 -0400

    adding stuff for cleaning the heiroglyphics

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# load the training data
+git submodule update --init --recursive
+
+# unzip training data
+unzip  hiero-transformer/training_data.zip -d data/egypt_char
+unzip  hiero-transformer/test_and_validation_data.zip -d data/egypt_char
+
+
+# install the required packages for transformers
+pip install torch numpy transformers datasets tiktoken wandb tqdm
+pip install triton
+
+
+pip install matviz
