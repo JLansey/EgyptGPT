aa6ce5ee069d9e3c0725d3b0aac7aa4f27bb8e5e
Jonathan Lansey <lansey+github@gmail.com>
2026-03-23 01:34:24 -0400

    getting the researcher access to the GPU

--- setup.sh
+++ setup.sh
@@ -7,8 +7,9 @@ unzip  hiero_transformer/test_and_validation_data.zip -d data/egypt_char
 
 
 # install the required packages for transformers
-pip install torch numpy transformers datasets tiktoken wandb tqdm
-pip install triton
+# NOTE: On Colab, PyTorch is pre-installed with CUDA. Do NOT install torch here
+# or it will overwrite the CUDA version with a CPU-only build.
+pip install numpy transformers datasets tiktoken wandb tqdm triton
 
 # code for converting the gardiner code to unicode hieroglyphs
 pip install gardiner2unicode
